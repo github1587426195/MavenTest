1e4d668bc360e4870cfe675d76b0062fe3764867
github1587426195 <819438030@qq.com>
2019-11-16 14:01:38 +0800

    init

---
+++ src/main/java/com/itgodo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.itgodo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.json.JSONArray;
+
+
+public class TestController {
+	public static void main(String[] args) {
+		List<String> list=new ArrayList<String>();
+		list.add("sss");
+		list.add("bbb");
+		
+		JSONArray array=JSONArray.fromObject(list);
+		
+		System.out.println(array.toString());
+		
+		list.forEach(o->{System.out.println(o);});
+	}
+}
